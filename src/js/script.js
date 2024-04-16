06449a769dbf1d77e6799301dd1aa89bbd95e35f
Front-Eugene <front.developer@mail.ru>
2024-04-16 13:18:22 +0300

    PC version

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", function() {
+    
+    // МАСКА ТЕЛЕФОНА
     [].forEach.call( document.querySelectorAll('.tel'), function(input) {
         var keyCode;
         function mask(event) {
@@ -37,39 +39,127 @@ window.addEventListener("DOMContentLoaded", function() {
     
     });
 
+    // МАСКА ТЕЛЕФОНА
+
+
+    // ОТПРАВКА ФОРМЫ
 
     const TOKEN = '7195270180:AAHRHTgMI77pAZG59Iw_6eRFfISkp4E68HM';
     const CHAT_ID = '-1002064961275';
     const URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
 
-    document.getElementById('contactForm').addEventListener('submit', function(e){
-        e.preventDefault()
-
+    document.getElementById('contactForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+      
+        // Get the values from the form fields
+        const name = this.name.value.trim();
+        const metr = this.metr.value.trim();
+        const phone = this.phone.value.trim();
+      
+        // Validate the form fields
+        if (name === '' || metr === '' || phone === '') {
+          // Display an error message or perform any other necessary action
+          alert('Пожалуйста заполните все поля.');
+          return;
+        }
+      
         let message = `<b>ЗАЯВКА С САЙТА</b>\n`;
-        message += `<b>ИМЯ:</b> ${ this.name.value }\n`;
-        message += `<b>ПЛОЩАДЬ:</b> ${ this.metr.value }\n`;
-        message += `<b>ТЕЛЕФОН:</b> ${ this.phone.value }`;
-
-        axios.post(URI_API, {
+        message += `<b>ИМЯ:</b> ${name}\n`;
+        message += `<b>ПЛОЩАДЬ:</b> ${metr}\n`;
+        message += `<b>ТЕЛЕФОН:</b> ${phone}`;
+      
+        axios
+          .post(URI_API, {
             chat_id: CHAT_ID,
             parse_mode: 'html',
             text: message
-        })
-
-        .then(() => {
-            this.name.value = ''
-            this.metr.value = ''
-            this.phone.value = ''
-        })
-
-        .catch((err) => {
+          })
+          .then(() => {
+            this.name.value = '';
+            this.metr.value = '';
+            this.phone.value = '';
+            alert('Мы свяжемся с вами в жлизжайшее время.');
+          })
+          .catch((err) => {
             console.log(err);
-        })
-
-        .finally(() => {
+          })
+          .finally(() => {
             console.log('КОНЕЦ');
-        })
-    })
+          });
+      });
+
+    // ОТПРАВКА ФОРМЫ
+
+
+    // ПОЛУЧЕНИЕ КЛАССА КАРТЫ
+
+    // Get all product cards
+    const productCards = document.querySelectorAll('.card');
+
+    // Get all buttons inside the product cards
+    const buttons = document.querySelectorAll('.card .product-btn');
+
+    // Get all close buttons inside the product cards
+    const closeButtons = document.querySelectorAll('.card .close');
+
+    const contactButtons = document.querySelectorAll('.card .close-card');
+
+    // Add click event listeners to each button
+    buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // Add the "more" class to the corresponding product card
+        productCards[index].classList.add('more');
+        
+        // Apply additional styles to the product card
+        // Add your custom styles here
+    });
+    });
+
+    // Add click event listeners to each close button
+    closeButtons.forEach((closeButton, index) => {
+    closeButton.addEventListener('click', () => {
+        // Remove the "more" class from the corresponding product card
+        productCards[index].classList.remove('more');
+        
+        // Remove any additional styles previously applied to the product card
+        // Add your custom styles here
+    });
+    });
+
+    // Add click event listeners to each close button
+    contactButtons.forEach((contactButton, index) => {
+      contactButton.addEventListener('click', () => {
+          // Remove the "more" class from the corresponding product card
+          productCards[index].classList.remove('more');
+          
+          // Remove any additional styles previously applied to the product card
+          // Add your custom styles here
+      });
+      });
+
+    // ПОЛУЧЕНИЕ КЛАССА КАРТЫ
+
+
+    // АКТИВНЫЙ ДОБОРНЫЙ МАТЕРИАЛ
+
+    const labels = document.querySelectorAll('.dop-material-items label');
+    const images = document.querySelectorAll('.dop-material-img');
+
+    labels.forEach((label, index) => {
+      label.addEventListener('click', () => {
+        // Hide all images
+        images.forEach((image) => {
+          image.style.display = 'none';
+          image.style.zIndex = '-1';
+        });
+
+        // Show the corresponding image
+          images[index].style.display = 'flex';
+          images[index].style.zIndex = '1';
+      });
+    });
+
+    // АКТИВНЫЙ ДОБОРНЫЙ МАТЕРИАЛ
 
 })
 
